d54c1f7eec63f82ef24f3c479924887f641fd84b
sharylizareid <59847086+sharylizareid@users.noreply.github.com>
2022-10-03 00:25:49 -0500

    sandbox update

--- deploy/sandbox.js
+++ deploy/sandbox.js
@@ -13,7 +13,7 @@ AWS.config.loadFromPath(__dirname + '/../aws-credentials.json');
 
 const extQ = `<?xml version="1.0" encoding="UTF-8"?>
                     <ExternalQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2006-07-14/ExternalQuestion.xsd">
-                        <ExternalURL>${process.env.npm_package_url}</ExternalURL>
+                        <ExternalURL>https://cse-256-f22.github.io/information-foraging-project-will-moises-sharyliz/</ExternalURL>
                         <FrameHeight>0</FrameHeight>
                     </ExternalQuestion>`;
 
